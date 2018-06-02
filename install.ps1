c2245d03f34c83da2c0f1d86f7d861637c7751e7
merkliopas <sarunasmerkliopa@gmail.com>
2018-06-02 11:04:10 +0300

    copying start script and icon to local destination

--- install.ps1
+++ install.ps1
@@ -13,16 +13,19 @@ Write-Output "Installation time taken: $((Get-Date).Subtract($start_time).Second
 Remove-Item -path $output
 
 Write-Output "Creating shortcut for terminator"
+copy $PSScriptRoot\icon\terminator.ico "C:\Users\$env:UserName\AppData"
+copy $PSScriptRoot\system\StartTerminator.vbs "C:\Users\$env:UserName\AppData"
+
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Terminator.lnk")
 $Shortcut.TargetPath =  "C:\Windows\System32\wscript.exe"
-$Shortcut.Arguments = "$PSScriptRoot\system\StartTerminator.vbs"
-$Shortcut.IconLocation = "$PSScriptRoot\icon\terminator.ico"
+$Shortcut.Arguments = "C:\Users\$env:UserName\AppData\StartTerminator.vbs"
+$Shortcut.IconLocation = "C:\Users\$env:UserName\AppData\terminator.ico"
 $Shortcut.WorkingDirectory = "%USERPROFILE%"
 $Shortcut.Save()
 
 Write-Output "Creating startup job to run x-server"
-copy $PSScriptRoot/system/StartXserver.bat "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy $PSScriptRoot\system\StartXserver.bat "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 Write-Output "Closing in 10 seconds"
 Start-Sleep -s 10
